53fc3554c863708eb50daf18f489e16f3073b02e
didajapa <116636770+didajapa@users.noreply.github.com>
2022-11-16 09:02:43 +0100

    Add comments in code

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 
+// Random number between 1-3 for computer choice, return 'rock', 'paper' or 'scissors'
 function getComputerChoice(min, max){
     let x = Math.floor(Math.random()*(max-min + 1)) + min;
-    console.log(x);
     if (x === 1) {
-    document.getElementById("demo").innerHTML = "rock";
-    return "rock";
+        document.getElementById("computer").innerHTML = "rock";
+        return "rock";
     } else if (x === 2) {
-        document.getElementById("demo").innerHTML = "paper";
+        document.getElementById("computer").innerHTML = "paper";
         return "paper";
     } else if (x === 3) {
-        document.getElementById("demo").innerHTML = "scissors";
+        document.getElementById("computer").innerHTML = "scissors";
         return "scissors";
     }
 }
 
+// Player choice via prompt
+let playerSelection = prompt("Rock, paper or scissors?");
+console.log(playerSelection);
 
-// function playRound(playerSelection, computerSelection){
+// Play round of game. Rock defeats scissors, scissors defeat paper, paper defeats rock
+// Return string that says if the player wins or loses.
+ function playRound(playerSelection, computerSelection){
 
 
-// }
+ }
 
+// Game consists of 5 rounds.
 // const playerSelection = "rock";
 // const computerSelection = getComputerChoice();
 // console.log(playRound(playerSelection, computerSelection));
